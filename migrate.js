c60e763294a95e9d06ab52a211b241bce851ba4f
Haluk Isik <isikhaluk@gmail.com>
2024-08-06 01:50:50 +0300

    Added README file

--- migrate.js
+++
@@ -1,9 +0,0 @@
-import { exec } from 'child_process';
-
-exec('prisma migrate deploy', (err, stdout, stderr) => {
-  if (err) {
-    console.error(`Error running migrations: ${err}`);
-    return;
-  }
-  console.log(`Migration output: ${stdout}`);
-});
--- prisma/migrations/20240802223716_place_name_optional/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE `Location` MODIFY `placeName` VARCHAR(191) NULL;
